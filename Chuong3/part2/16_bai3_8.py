fc4684e63665421862058b63cbb8a99c8e335815
tkieuvt <thanhkieu13052004@gmail.com>
2023-03-10 07:57:31 +0700

    Cslt

--- Chuong3/part2/16_bai3_8.py
+++ Chuong3/part2/16_bai3_8.py
@@ -1,6 +1,7 @@
 s=input()
+s+=" "
 n=int(input())
 for i in range(1,n+1,1):
-    for j in range(1,i+1,1):
-        print(s,end=" ")
-    print()
+    #for j in range(1,i+1,1):
+        print(s*i)
+#print()
