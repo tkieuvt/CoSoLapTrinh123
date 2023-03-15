fc1bb90e222ec9db87e4c642893cffc0eacb0e7f
tkieuvt <thanhkieu13052004@gmail.com>
2023-03-15 22:56:12 +0700

    aa

---
+++ Chuong4/slide.py
@@ -0,0 +1,16 @@
+'''#slide_17
+def show_number(n=10):
+    for i in range(1,n+1):
+        print(i)
+show_number(5)
+show_number()'''
+'''#slide_20
+def phepcong(x , y = None):
+    if y == None:
+        return x
+    else: return x + y
+kq1 = phepcong(2)
+kq2 = phepcong(5,2)
+print(kq1,kq2)'''
+
+
---
+++ Chuong4/slide6.py
@@ -0,0 +1,11 @@
+def nhap():
+    print("Nhap mot so nguyen:")
+    n=int(input("n="))
+    return n
+def tinh(n):
+    s=0
+    for x in range(1,n+1):
+        s=s+x
+    return s
+def inkq(n,s):
+    print("Tong cua ",n," so nguyen duong dau tien=",s,sep="")
