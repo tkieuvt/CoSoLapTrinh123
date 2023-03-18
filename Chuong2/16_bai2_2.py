f4b27e09a9f7cc7d175605cfe42c6869a7c521b6
tkieuvt <thanhkieu13052004@gmail.com>
2023-03-18 21:24:18 +0700

    C2

--- Chuong2/16_bai2_2.py
+++ Chuong2/16_bai2_2.py
@@ -1,4 +1,6 @@
 r=int(input("Nhap vao ban kinh cua duong tron:"))
-pi=3.14;s=pi*r*r;c=2*r*pi
+pi=3.14
+s=pi*r*r
+c=2*r*pi
 print("Dien tich cua duong tron co ban kinh "+str(r)+" la =",round(s,1))
 print("Chu vi cua duong tron co ban kinh "+str(r)+" la =",round(c,1))
--- Chuong3/part2/16_bai-2.py
+++ Chuong3/part2/16_bai-2.py
@@ -1,6 +1,9 @@
-n=int(input("n="))
+'''n=int(input("n="))
 d=0
 for i in range(2,n,1):
     if n%i==0: d=d+1
 if d==0: print(n,"la SNT")
-else: print(n,"khong la SNT")
+else: print(n,"khong la SNT")'''
+s=input("n=")
+d=len(s)
+print(d)
