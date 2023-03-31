88aa46b175bec0973c00be2534f6976d76e4465c
tkieuvt <thanhkieu13052004@gmail.com>
2023-03-31 20:43:52 +0700

    M

--- Chuong4/16_bai4_2.py
+++ Chuong4/16_bai4_2.py
@@ -1,23 +1,24 @@
-'''def inkq(n):
+def inkq(n):
     for i in range(2,n+1,2): 
-        print(i)
+        print(i,end=" ")
+    print()
 def nhap():
     n=int(input("n="))
     inkq(n)
 t=""
 while (t!="k" and t!="K"):
     n=nhap()
-    t=input("Tiep tuc khong?")'''
-
-
-# dieu kien nhap long def 
-def inkq(n):
-    for i in range(2,n,2): 
-        print(i)
-def nhap():
-    n=int(input("n="))
-    inkq(n)
-    # t=""
     t=input("Tiep tuc khong?")
-    if (t!="k" and t!="K"): nhap()
-nhap()
+
+
+# # dieu kien nhap long def 
+# def inkq(n):
+#     for i in range(2,n+2,2): 
+#         print(i,end=" ")
+# def nhap():
+#     n=int(input("n="))
+#     inkq(n)
+#     # t=""
+#     t=input("Tiep tuc khong?")
+#     if (t!="k" and t!="K"): nhap()
+# nhap()
--- Chuong4/slide34.py
+++ Chuong4/slide34.py
@@ -1,9 +1,14 @@
-def nhap():
-    x=int(input("x="))
-    global y 
-    y=5
-    return x+y
+# def nhap():
+#     x=int(input("x="))
+#     global y 
+#     y=5
+#     return x+y
 
-kq=nhap()
-print("y=",y,sep="")
-print("kq=",kq,sep="")
+# kq=nhap()
+# print("y=",y,sep="")
+# print("kq=",kq,sep="")
+def inkq(n):
+    for i in range(2,n+1,2):
+        print(i)
+n=int(input())
+inkq(n)
